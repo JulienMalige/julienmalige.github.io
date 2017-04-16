61718ba839e641f821a02529194d70695bab9aa2
julienmalige <julienmalige@gmail.com>
2017-04-16 13:55:50 +0200

    Typed Title

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    Typed.new(".typed", {
+        strings: ["Data", "ML", "IoT", "Cloud", "Devs", "Techs"],
+        loop: true,
+        backDelay: 3000,
+        typeSpeed: 150,
+        backSpeed: 150
+    });
+});
